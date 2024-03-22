ceba42283aae00e507714b78ed5a261e44f6e9b6
Chori <yidodeveloper@gmail.com>
2024-03-22 20:03:31 +0900

    Feat: 에세이에 대해 평가한 피드백 모델 구현

--- app/models.py
+++ app/models.py
@@ -2,3 +2,6 @@ from django.db import models
 
 class Essay(models.Model):
     essayContent = models.CharField(max_length=1000)
+
+class Feedback(models.Model):
+    feedbackContent = models.CharField
